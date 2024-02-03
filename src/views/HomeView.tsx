f707d303e0c71e13b1b253f2619da2fedc5e7a09
Cristhian Pabon <crispycode@Cristhians-MacBook-Pro.local>
2024-02-03 16:46:52 -0300

    initial commit..

---
+++ src/views/HomeView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomeView() {
+  return (
+    <>
+      <h1>Soy un titulo </h1>
+      <p>Agrega aqui todo lo que desees ver en la home...</p>
+    </>
+  );
+}
+
+export default HomeView;
